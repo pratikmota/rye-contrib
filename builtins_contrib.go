e591954b85b98c5fd04ad096c3336dde7b0c3fb2
jimbo <janko.itm@gmail.com>
2022-07-28 02:24:21 +0200

    added demo contrib to test if it works

---
+++ builtins_contrib.go
@@ -0,0 +1,74 @@
+// +build b_tiny
+
+package contrib
+
+import (
+	"rye/env"
+	"rye/evaldo"
+	"strings"
+)
+
+var Builtins_contrib = map[string]*env.Builtin{
+
+	"contrib-one": {
+		Argsn: 1,
+		Fn: func(env1 *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+			return env.Integer{1}
+		},
+	},
+
+	"contrib-do": {
+		Argsn: 1,
+		Doc:   "Takes a block of code and does (runs) it.",
+		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+			switch bloc := arg0.(type) {
+			case env.Block:
+				ser := ps.Ser
+				ps.Ser = bloc.Series
+				evaldo.EvalBlock(ps)
+				ps.Ser = ser
+				return ps.Res
+			}
+			return nil
+		},
+	},
+}
+
+func RegisterBuiltins(ps *env.ProgramState) {
+	builtinNames = make(map[string]int)
+	RegisterBuiltins2(Builtins_contrib, ps, "contrib")
+}
+
+var builtinNames map[string]int
+
+func RegisterBuiltins2(builtins map[string]*env.Builtin, ps *env.ProgramState, name string) {
+	builtinNames[name] = len(builtins)
+	for k, v := range builtins {
+		bu := env.NewBuiltin(v.Fn, v.Argsn, v.AcceptFailure, v.Pure, v.Doc)
+		registerBuiltin(ps, k, *bu)
+	}
+}
+
+func registerBuiltin(ps *env.ProgramState, word string, builtin env.Builtin) {
+	// indexWord
+	// TODO -- this with string separator is a temporary way of how we define generic builtins
+	// in future a map will probably not be a map but an array and builtin will also support the Kind value
+
+	idxk := 0
+	if strings.Index(word, "//") > 0 {
+		temp := strings.Split(word, "//")
+		word = temp[1]
+		idxk = ps.Idx.IndexWord(temp[0])
+	}
+	idxw := ps.Idx.IndexWord(word)
+	// set global word with builtin
+	if idxk == 0 {
+		ps.Ctx.Set(idxw, builtin)
+		if builtin.Pure {
+			ps.PCtx.Set(idxw, builtin)
+		}
+
+	} else {
+		ps.Gen.Set(idxk, idxw, builtin)
+	}
+}
---
+++ builtins_contrib_not.go
@@ -0,0 +1,9 @@
+// +build !b_tiny
+
+package contrib
+
+import (
+	"rye/env"
+)
+
+var Builtins_contrib = map[string]*env.Builtin{}
